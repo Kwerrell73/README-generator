9e2817303bda0d20e912937e254c0cdc8826f179
Kellie Werrell <kelliewerrell@gmail.com>
2021-05-08 12:14:03 -0700

    added question and function to initilize app

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,92 @@ const inquirer = require('inquirer');
 const generateMarkDown = require("./utils/generateMarkDown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'what is your GitHub username? (Reuqired)',
+        name: 'username',
+        validate: (answer) => {
+            if (answer) {
+                return true;                                
+            } else {
+                 console.log("A valid GitHub username is required.");
+            }
+            
+        }
+    },
+
+    {
+        type: 'input',
+        message: 'What is the title of your project? (Required)',
+        name: 'title',
+        validate: (answer) => {
+            if (answer) {
+                return true;                                
+            } else {
+                 console.log("A valid project title is required.");
+            }
+            
+        }
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide a description of your project. (Required)',
+        name: 'description',
+       
+        validate: (answer)  => {
+            if (answer) {
+                return true;                                
+            } else {
+                 console.log("A valid project description is required.");
+            }
+            
+        }
+    },
+
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did languages did you use to create this project? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
+      },
+
+
+
+    
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {   
+        console.log("Generate ReadMe");
+        fs.writeFileSync("ReadMe.md", inquirerResponse, data);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
 
 // Function call to initialize app
 init();
