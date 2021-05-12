2abd6cde1f44c6fdd8663cd4c36243b4bd487cbb
Kellie Werrell <kelliewerrell@gmail.com>
2021-05-12 10:47:37 -0700

    added writefile and section to create readme

---
+++ Develop/dragonfly.md
@@ -0,0 +1,23 @@
+# dragonfly
+    
+   * [username](#username) 
+   * [title](#title) 
+   * [description](#description) 
+   * [languages](#languages) 
+   * [licenses](#licenses) 
+   * [contribution](#contribution) 
+   * [link](#link) 
+   ## username
+    Kwerrell73
+    ## title
+    dragonfly
+    ## description
+    school homework
+    ## languages
+    JavaScript,HTML,CSS,ES6,jQuery,Bootstrap,Node
+    ## licenses
+    mit
+    ## contribution
+    me
+    ## link
+    link
--- Develop/index.js
+++ Develop/index.js
@@ -4,137 +4,133 @@ const inquirer = require('inquirer');
 const generateMarkDown = require("./utils/generateMarkDown");
 
 // TODO: Create an array of questions for user input
-const readmeQuestions = [
-    {
-        type: 'input',
-        message: 'what is your GitHub username? (Required)',
-        name: 'username',
-        validate: (answer) => {
-            if (answer) {
-                return true;                                
-            } else {
-                 console.log("A valid GitHub username is required.");
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            message: 'what is your GitHub username? (Required)',
+            name: 'username',
+            validate: (answer) => {
+                if (answer) {
+                    return true;
+                } else {
+                    console.log("A valid GitHub username is required.");
+                }
+
             }
-            
-        }
-    },
+        },
 
-    {
-        type: 'input',
-        message: 'What is the title of your project? (Required)',
-        name: 'title',
-        validate: (answer) => {
-            if (answer) {
-                return true;                                
-            } else {
-                 console.log("A valid project title is required.");
+        {
+            type: 'input',
+            message: 'What is the title of your project? (Required)',
+            name: 'title',
+            validate: (answer) => {
+                if (answer) {
+                    return true;
+                } else {
+                    console.log("A valid project title is required.");
+                }
+
             }
-            
-        }
-    },
+        },
 
-    {
-        type: 'input',
-        message: 'Please provide a description of your project. (Required)',
-        name: 'description',
-       
-        validate: (answer)  => {
-            if (answer) {
-                return true;                                
-            } else {
-                 console.log("A valid project description is required.");
+        {
+            type: 'input',
+            message: 'Please provide a description of your project. (Required)',
+            name: 'description',
+
+            validate: (answer) => {
+                if (answer) {
+                    return true;
+                } else {
+                    console.log("A valid project description is required.");
+                }
+
             }
-            
-        }
-    },
+        },
 
-    {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What did languages did you use to create this project? (Check all that apply)',
-        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-      },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did languages did you use to create this project? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'],
+        },
 
-     // {
-     //   type: 'input',
-      //  name: 'link',
-      //  message: 'Enter the GitHub link to your project. (Required)',
-      //  validate: linkInput => {
-      //    if (linkInput) {
-      //      return true;
-      //    } else {
-      //      console.log('You need to enter a project GitHub link!');
-      //     return false;
-       //   }
-   //     }
-   //   },
+        {
+            type: 'input',
+            message: "What licenses are required with this project?",
+            name: "licenses"
+
+        },
+
+        {
+            type: 'input',
+            message: "Who were the contributors to this project?",
+            name: "contribution"
+        },
+
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a project GitHub link!');
+                    return false;
+                }
+            }
+        },
 
 
-
+    ]
+).then(({
+    username,
+    title,
+    description,
+    languages,
+    licenses,
+    contribution,
+    link
+})=>{
+    const template =`# ${title}
     
-];
+   * [username](#username) 
+   * [title](#title) 
+   * [description](#description) 
+   * [languages](#languages) 
+   * [licenses](#licenses) 
+   * [contribution](#contribution) 
+   * [link](#link) 
+   ## username
+    ${username}
+    ## title
+    ${title}
+    ## description
+    ${description}
+    ## languages
+    ${languages}
+    ## licenses
+    ${licenses}
+    ## contribution
+    ${contribution}
+    ## link
+    ${link}`
+    
+writeToFile(title, template);
+}
+);
+
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {  return `
 
-# ${response.title}
-
-# Table of Content
--[description](#description)
--[installation](#installation)
--[usage](#usage)
--[licenses](#licenses)
--[contribution](#contribution)
--[test](#test)
--[username](#username)
--[profile](#profile)
-
-${response.username}
-##username:
-
-    ${response.description}
-##description:
-
-    ${response.installation}
-##installation:
-
-    ${response.usage}
-##usage:
-
-    ${response.licenses}
-##licenses:
-
-    ${response.contribution}
-##contribution:
-
-    ${response.test}
-##test:
-
-    ${response.email}
-##email:
-
-    ${response.profile}
-##profile:
-`;
-}
-
-module.exports = generateMarkdown;
-
-
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(readmeQuestions)
-    .then((inquirerResponse, data) => {   
-        console.log("Generate ReadMe");
-        fs.writeFileSync("ReadMe.md", inquirerResponse, data);
-    })
-    .catch((err) => {
-        console.log(err);
+function writeToFile(fileName, data) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`,data,(err)=>{
+        if(err){
+            console.log(err)
+        }
+            console.log('README has been generated'); 
     })
 }
-
-
-// Function call to initialize app
-init();
